99f2e16f48d56afbd11e4ec4bc11429a1b3a9c43
Mariana M <mariana_montoya82221@elpoli.edu.co>
2023-05-29 21:02:00 -0500

    Ingreso válido

--- src/utilidades/Vista.java
+++ src/utilidades/Vista.java
@@ -1,49 +1,57 @@
-
 package utilidades;
+
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import javax.swing.JOptionPane;
 import logica.Grafo;
 import utilidades.Validar;
 
-
 public class Vista {
-    Validar v= new Validar();
-    Grafo G= new Grafo();
-    
-    public void VistaIngreso()
-    {
+
+    Validar v = new Validar();
+    Grafo G = new Grafo();
+
+    public void VistaIngreso() {
         String Sl, Sv = v.ValidarString("Ingrese vértices a agregar al grafo (Separados mediante comas)");
-        int i, la [], ve[];
+        int i, la[], ve[];
         JOptionPane.showMessageDialog(null, Sv, "", 0);
         Sl = v.ValidarString(""" 
                                 Ingrese las aristas del grafo y su respectivo peso (Separados mediante comas)
                                 Por ejemplo: (1,2,4) (1,3,5)
                                     """);
-                                                   
+
         JOptionPane.showMessageDialog(null, Sl, "", 0);
-        la= CadenaAvector(Sl);
-        ve=CadenaAvector(Sv);
-        IngresoValido(ve,la);
-        G.CrearMA(ve, la);     
+        la = CadenaAvector(Sl);
+        ve = CadenaAvector(Sv);
+        while (!IngresoValido(ve, la))
+        {
+            JOptionPane.showMessageDialog(null, Sv, "Vertices ingresados", 3);
+            Sl = v.ValidarString(""" 
+                                Ingrese las aristas del grafo y su respectivo peso (Separados mediante comas)
+                                Por ejemplo: (1,2,4) (1,3,5)
+                                   """);
+        }
+        G.CrearMA(ve, la);
     }
-    public void VistaMostrar()
-    {
-        
+
+    public void VistaMostrar() {
+
     }
-    
-    
-        private boolean IngresoValido(int[] ve, int[] la) {
+
+    private boolean IngresoValido(int[] ve, int[] la) {
         boolean b = false;
-        int cont = 1;
+        int cont = 0;
+        List<Integer> veL = Arrays.stream(ve).boxed().collect(Collectors.toList());
         if (la.length % 3 == 0)
         {
             for (int i = 0; i < la.length; i++)
             {
-               
-                
+                cont++;
+
                 if (cont % 3 != 0)
                 {
-                    if (Arrays.asList(ve).contains(la[i]))
+                    if (veL.contains(la[i]))
                     {
                         b = true;
                     } else
@@ -52,7 +60,7 @@ public class Vista {
                         return false;
                     }
                 }
-                cont++;
+
             }
         } else
         {
@@ -62,12 +70,10 @@ public class Vista {
     }
 
     private int[] CadenaAvector(String s) {
-        
-        
         int[] ve = new int[s.length()];
         for (int i = 0; i < s.length(); i++)
         {
-            ve[i] = s.charAt(i)-'0';
+            ve[i] = s.charAt(i) - '0';
 
         }
         return ve;
